73d3fe4e482c913fd44428a0679323597de4a627
KakashiSensei <vivekfitkariwala@gmail.com>
2017-07-01 10:22:37 +0530

    Updating AppID

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let config = {
     restAPI: "https://white-light-rest-api.herokuapp.com",
-    appID: "1866917183572616",
+    appID: "399964337042548",
     scope: "public_profile,email,user_photos,user_posts,user_friends"
 }
 
